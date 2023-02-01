6aa413c212b689b4991212ab927c8e5a7994f405
Anton.gap <anton.gap99@gmail.com>
2023-02-01 14:28:18 +0300

    some changes

--- src/script/map.js
+++ src/script/map.js
@@ -1,24 +1,29 @@
 export const mapController = () => {
-    const initMap = () => {
-        const myMap = new ymaps.Map(
-            'map',
-            {
-                center: [51.014760, 3.586506],
-                zoom: 12,
-                controls: ['smallMapDefaultSet'],
-            },
-            {},
-        );
-        const myPlacemark = new ymaps.Placemark(
-            [55.7724, 37.6252],
-            {},
-            {
-                iconLayout: 'default#image',
-                iconImageSize: [70, 70],
-                iconImageOffset: [-35, -70],
-            },
-        );
-        myMap.geoObjects.add(myPlacemark);
+    try {
+        const initMap = () => {
+            const myMap = new ymaps.Map(
+                'map',
+                {
+                    center: [51.014760, 3.586506],
+                    zoom: 12,
+                    controls: ['smallMapDefaultSet'],
+                },
+                {},
+            );
+            const myPlacemark = new ymaps.Placemark(
+                [55.7724, 37.6252],
+                {},
+                {
+                    iconLayout: 'default#image',
+                    iconImageSize: [70, 70],
+                    iconImageOffset: [-35, -70],
+                },
+            );
+            myMap.geoObjects.add(myPlacemark);
+        }
+        ymaps.ready(initMap);
+    } catch (error) {
+        console.log('проблемы с картами');
     }
-    ymaps.ready(initMap);
+
 };
--- src/script/swiperController.js
+++ src/script/swiperController.js
@@ -1,25 +1,38 @@
 import Swiper from "swiper";
 
 export const swiperController = () => {
-    new Swiper('.goede__swiper', {
+  new Swiper(".goede__swiper", {
+    navigation: {
+      nextEl: ".goede__button-next",
+      prevEl: ".goede__button-prev",
+    },
+  });
 
-        navigation: {
-            nextEl: '.goede__button-next',
-            prevEl: '.goede__button-prev',
-        },
-
-    });
-
-    new Swiper('.Sfeerfoto__slider', {
-        loop: true,
-        centeredSlides: 1,
-        initialSlide: 3,
-        slidesPerView: 3.15,
+  new Swiper(".Sfeerfoto__slider", {
+    loop: true,
+    // centeredSlides: true,
+    slidesPerView: 1.98,
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1170: {
+        slidesPerView: 4,
         spaceBetween: 30,
-        navigation: {
-            nextEl: '.Sfeerfoto__button-next',
-            prevEl: '.Sfeerfoto__button-prev',
-        },
-
-    });
-}
+      },
+      1172: {
+        slidesPerView: 1.98,
+        spaceBetween: 30,
+      },
+      1174: {
+        slidesPerView: 2,
+      },
+    },
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".Sfeerfoto__button-next",
+      prevEl: ".Sfeerfoto__button-prev",
+    },
+  });
+};
